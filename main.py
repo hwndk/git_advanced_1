cc19dfad6e1789c0c224142712499aec52fee1d6
hwndk <dongha894@gmail.com>
2023-10-18 10:29:15 +0000

    sum add

---
+++ main.py
@@ -0,0 +1,19 @@
+def even_list(int_list: List[int]) -> List[int]:
+    pass
+
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    res = 0
+
+    for i in range(len(even_int_list)):
+        res += even_int_list[i]
+
+    return res
+
+
+def main():
+    int_list = [1,2,3,4,5,6,7,8,9,10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
+if __name__ = "__main__": main()
